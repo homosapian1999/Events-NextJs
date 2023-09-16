42751d0fc3853ae6b8c5799f1212105cc9888b6c
Your GitHub Username <Your GitHub Email Address>
2023-09-16 22:02:44 +0530

    Basic Setup

---
+++ pages/events/[...slug].js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FilteredEventPage = () => {
+  return (
+    <div>
+      <h1>Filtered Event Page</h1>
+    </div>
+  );
+};
+
+export default FilteredEventPage;
