05973d4c84aae677e60dfdbd642369fb2ae4a7a4
Your GitHub Username <Your GitHub Email Address>
2023-09-17 06:42:04 +0530

    Add Basic routes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-import React from "react";
+import EventList from "@/components/events/event-list";
+import { getFeaturedEvents } from "../dummy-data";
 
 const HomePage = () => {
+  const featureEvents = getFeaturedEvents();
+
   return (
     <div>
       <h1>Hello Events</h1>
+      <EventList items={featureEvents} />
     </div>
   );
 };
